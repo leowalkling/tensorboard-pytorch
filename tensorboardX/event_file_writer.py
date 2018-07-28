0b84bfeeae6e44ed5442899bced2123f82e8dec6
Leopold Walkling <leopold.walkling@bmw.de>
2018-07-28 15:12:35 +0200

    Enabling continuing to write into the same file

--- tensorboardX/event_file_writer.py
+++ tensorboardX/event_file_writer.py
@@ -40,8 +40,7 @@ class EventsWriter(object):
         Events files have a name of the form
         '/some/file/path/events.out.tfevents.[timestamp].[hostname]'
         '''
-        self._file_name = file_prefix + ".out.tfevents." + str(time.time())[:10] + "." +\
-            socket.gethostname() + filename_suffix
+        self._file_prefix = file_prefix + ".out.tfevents" + filename_suffix
 
         # Open(Create) the log file with the particular form of name.
         logging.basicConfig(filename=self._file_name)
